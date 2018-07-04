153b6b4c32c5cd4235e9b8de6f1b00148365e59b
naokinagaishi-ffs <38636999+naokinagaishi-ffs@users.noreply.github.com>
2018-07-04 21:49:55 +0900

    とりあえず

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,27 +9,23 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-      
-           
+        {
             IPersonDataAccesor dao = new CSVPerson();
-           
-
             var workerlist = dao.GetPerson();
-
 
             ////お小遣いの平均値をコンソールに表示
             Console.WriteLine("小遣いの平均値は,{0}円です", Calculater.Average(workerlist));
 
             //お小遣いが一番多い人の名前と金額
             var richpeople = Calculater.SearchRichPerson(workerlist);
-            foreach( var p in richpeople)
+            foreach (var p in richpeople)
             {
                 Console.WriteLine("お小遣いが一番多い人は，{0}さんで，{1}円です。", p.name, p.money.ToString());
             }
 
             //お小遣いが一番少ない人の名前と金額
             var poorpeople = Calculater.SearchPoorPerson(workerlist);
-            foreach( var p in poorpeople)
+            foreach (var p in poorpeople)
             {
                 Console.WriteLine("お小遣いが一番少ない人は，{0}さんで，{1}円です。", p.name, p.money.ToString());
             }
@@ -37,7 +33,7 @@ namespace ConsoleApp1
 
             //FFで要求①
             IEnumerable<Person> ff_datalist = workerlist.Where(x =>
-                                                  x.id.Substring(0,3)
+                                                  x.id.Substring(0, 3)
                                                   == "100");
             Console.WriteLine("FFの小遣いの平均値は,{0}円です", Calculater.Average(ff_datalist));
 
@@ -54,10 +50,10 @@ namespace ConsoleApp1
             }
             Console.WriteLine();
 
-            
+
             //FFSで要求①
             IEnumerable<Person> ffs_datalist = workerlist.Where(x =>
-                                                 x.id.ToString().Substring(0, 3)
+                                                 x.id.Substring(0, 3)
                                                  == "128");
             Console.WriteLine("FFSの小遣いの平均値は,{0}円です", Calculater.Average(ffs_datalist));
 
